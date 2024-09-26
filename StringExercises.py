cc435a0ce13ffb442fba87ff5242614772279971
berfutulgar <berfutulgar@gmail.com>
2024-09-26 22:30:19 +0300

    Convert even-indexed characters to uppercase in a string.

---
+++ StringExercises.py
@@ -0,0 +1,19 @@
+"""
+Changing strings:
+
+before: "hi my name is john and i am learning python"
+after: "Hi mY NaMe iS JoHn i aM LeArNiNg pYtHoN"
+
+"""
+
+def alternating(string):
+    new_string= ""
+    for string_index in range(len(string)):
+        if string_index %2==0:
+            new_string += string[string_index].upper()
+        else:
+            new_string += string[string_index].lower()
+    print(new_string)
+
+
+alternating("hi my name is john and i am learning python")
